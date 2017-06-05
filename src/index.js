b03401f9fbf6ef5974c7da3936cb7bb17392b73c
xudongdong <2281221979@qq.com>
2017-06-05 23:44:04 +0800

    webpack 配置

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by xudongdong-i on 2017/6/5.
+ */
+import Vue from '../node_modules/vue/dist/vue.js'
+import Favlist from './components/Favlist.vue'
+
+new Vue({
+	el: '#aaa',
+	components: { Favlist }
+})
